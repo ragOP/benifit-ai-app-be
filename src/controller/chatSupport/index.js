f6b141f1c42da22e7044098f9b3716210c175115
mdehtasham11 <md.01219011922@ipu.ac.in>
2025-08-30 15:23:11 +0530

    feat: enhance chat support by adding conversation handling and chat history retrieval

--- src/controller/chatSupport/index.js
+++ src/controller/chatSupport/index.js
@@ -3,6 +3,7 @@ const { ApiError } = require("../../utils/apiError/index.js");
 const { asyncHandler } = require("../../utils/asynchandler/index.js");
 const { createConversation } = require("../../services/chatSupport/index.js");
 const Message = require("../../models/chat/message/index.js");
+const Conversation = require("../../models/chat/conversation/index.js");
 
 exports.handleCreateChat = asyncHandler(async (req, res) => {
   const { userId, adminId } = req.body;
@@ -17,13 +18,40 @@ exports.handleCreateChat = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, result.data, result.message));
 });
 exports.handleMessages = asyncHandler(async (req, res) => {
-  const { text, userId, conversationId } = req.body;
-  if (!text || !userId || !conversationId) {
+  const { text, userId, conversationId, adminId } = req.body;
+  if (!userId) {
     new ApiResponse(400, null, "Enter valid details");
+  }
+  let conversation;
+  if (!conversationId) {
+    conversation = await createConversation(userId, adminId);
   }
 
   const result = await Message.create({ text, userId, conversationId });
   return res
     .status(200)
-    .json(new ApiResponse(200, result, "message created success"));
+    .json(
+      new ApiResponse(200, { result, conversation }, "message created success")
+    );
+});
+
+exports.handleGetChatHistory = asyncHandler(async (req, res) => {
+  const { conversationId } = req.params;
+  const conversation = await Conversation.findOne({ conversationId });
+  if (!conversation) {
+    new ApiResponse(400, null, "ConversationId Missing");
+  }
+  const message = await Message.find({ chatId: conversationId }).sort({
+    createdAt: 1,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, message, "message fetched successfully"));
+});
+exports.handleGetAllUser = asyncHandler(async (req, res) => {
+  const result = await Conversation.find();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, "all user fetched successfully"));
 });
--- src/routes/chatSupport/index.js
+++ src/routes/chatSupport/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const {
-  handleCreateChat,
   handleMessages,
+  handleGetChatHistory,
+  handleGetAllUser,
 } = require("../../controller/chatSupport/index.js");
 
 const Router = express.Router();
 
-Router.route("/create-chat").post(handleCreateChat);
+Router.route("/chat-history").get(handleGetChatHistory);
 Router.route("/send-message").post(handleMessages);
+Router.route("/get-user").post(handleGetAllUser);
 
 module.exports = Router;
